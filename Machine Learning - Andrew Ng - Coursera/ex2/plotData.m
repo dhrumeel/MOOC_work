61e7e7b8f111c03f2fa2b9df2d8a0080a1fee761
Dhrumeel <dhrumeel@gmail.com>
2017-05-29 23:41:23 -0700

    Added MATLAB code for Andrew Ng's Machine Learning Coursera MOOC.

---
+++ Machine Learning - Andrew Ng - Coursera/ex2/plotData.m
@@ -0,0 +1,31 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+neg = find(y==0);
+pos = find(y==1);
+% fprintf('Plotting %d pos points and %d neg points', size(y1_rows), size(y0_rows));
+
+% Plot the + points
+plot( X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 1.5, 'MarkerFaceColor', 'b', 'MarkerSize', 7 );
+
+% Plot the o points
+plot( X(neg, 1), X(neg, 2), 'ko', 'LineWidth', 1.5, 'MarkerFaceColor', 'y', 'MarkerSize', 7 );
+
+% =========================================================================
+
+
+
+hold off;
+
+end
